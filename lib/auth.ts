4bd9ce2b0844ca8382a3daaf245256354d9f3553
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-04-21 18:58:18 +0700

    feat: set firebase local persistence

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,16 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateCurrentUser } from "firebase/auth";
 import { auth } from "./firebase";
 
+import { browserLocalPersistence, setPersistence } from "firebase/auth";
+
 export const signup = async (email: string, password: string) => {
+    await setPersistence(auth, browserLocalPersistence);
     const user = await createUserWithEmailAndPassword(auth, email, password);
     return user;
 }
 
 export const signin = async (email: string, password: string) => {
+    await setPersistence(auth, browserLocalPersistence);
     const user = await signInWithEmailAndPassword(auth, email, password);
     return user;
 }
