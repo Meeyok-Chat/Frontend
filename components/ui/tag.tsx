89722a472da148d5caa3dc3b290a13699995b875
ThanaphomH <thanaphom.h@gmail.com>
2025-04-23 01:57:08 +0700

    fix: select-none tag

--- components/ui/tag.tsx
+++ components/ui/tag.tsx
@@ -9,7 +9,7 @@ export default function Tag({ className, type }: TagProps) {
   return (
     <p
       className={cn(
-        "text-xs px-1 py-0.5 rounded",
+        "text-xs px-1 py-0.5 rounded select-none",
         type === "Group"
           ? "bg-blue-100 text-blue-800"
           : "bg-green-100 text-green-800",
