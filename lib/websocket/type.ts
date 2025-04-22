910893752c8e5fdba45f6ff206244af2a5bdaae8
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-04-22 19:38:18 +0700

    feat: websocket type

--- lib/websocket/type.ts
+++ lib/websocket/type.ts
@@ -1,6 +1,9 @@
 export enum EventType {
   EVENT_SEND_MESSAGE = "send_message",
   EVENT_NEW_MESSAGE = "new_message",
+  EVENT_NEW_GROUP = "new_group",
+  EVENT_NEW_USER = "new_user",
+  EVENT_LEAVE_USER = "leave_user"
 }
 
 export type WSMessageEvent = {
@@ -9,6 +12,14 @@ export type WSMessageEvent = {
   from: string;
   createAt: string;
 };
+
+export type NewUserStatusEvent = {
+  user_id: string;
+}
+
+export type NewGroupEvent = {
+  chat_id: string;
+}
 
 export type WebSocketEvent = {
   type: EventType;
